cd7da99c991d8d69e8ede5424cac67f72039265c
silavsale <silavsale@gmail.com>
2020-03-14 22:37:15 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const STRIPE_PUBLISHABLE_KEY = process.env.STRIPE_PUBLISHABLE_KEY;
+require('dotenv').config();
+const express = require('express');
+const stripe = require('stripe')(STRIPE_PUBLISHABLE_KEY);
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars')
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
+
